e453465c6fcb78e61c7c26ce1d306345ab1c4500
red-patriot <realredpatriot@gmail.com>
2019-09-22 17:37:30 -0600

    Update cladder.el

--- cladder.el
+++ cladder.el
@@ -12,7 +12,7 @@
 ;; (at your option) any later version.
 
 ;; This program is distributed in the hope that it will be useful,
-n;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
